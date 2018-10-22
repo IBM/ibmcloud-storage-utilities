f3cda6e6b19d223185a0d602e980b2eb7339e755
jsilberm <jsilberm@gmail.com>
2018-10-22 09:12:46 -0700

    update/optimize

--- portworx-utils/docker-entrypoint.sh
+++ portworx-utils/docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+set -xv
+#
 # CLUSTER=`grep cluster: /data/yamlgen.yaml | awk '{print $2}'`
 # mkdir /etc/kubeadmin
 # cp /config/plugins/container-service/clusters/$CLUSTER/* /etc/kubeadmin 
 # export KUBECONFIG=`ls /etc/kubeadmin/*.yml`
 # echo KUBECONFIG=$KUBECONFIG
 # kubectl get nodes  || { echo "Can't access Kubernetes through KUBECONFIG=$KUBECONFIG" >&2 ; exit 1; }
-
 export IAMTOKEN=`cat /config/config.json | jq  ."IAMToken" | sed -e 's/"//g'`
+echo "IAMTOKEN = $IAMTOKEN"
+
 
 ./mkpvyaml
 
