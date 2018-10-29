36c943b075f705c1100afeaa67c7982f6059ddd9
nikolay <nikolay@administrators-MacBook-Pro.local>
2018-10-29 12:40:32 -0700

    Adding check for IAMAccessToken

--- portworx-utils/docker-entrypoint.sh
+++ portworx-utils/docker-entrypoint.sh
@@ -3,7 +3,9 @@
 #
 # echo "IAMTOKEN = $IAMTOKEN"
 
-export IAMTOKEN=`cat /config/config.json | jq  ."IAMToken" | sed -e 's/"//g'`
+if [ -z $IAMTOKEN ]; then
+    export IAMTOKEN=`cat /config/config.json | jq  ."IAMToken" | sed -e 's/"//g'`
+fi
 
 
 ./mkpvyaml.py
