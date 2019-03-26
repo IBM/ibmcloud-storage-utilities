06257d8cb69ff046592effa984c692174422fbb6
root <mumutyal@in.ibm.com>
2019-03-26 12:36:15 -0400

    Removed IBM KP

--- block-storage-attacher/tests/e2e/e2e-tests/utilscript.sh
+++ block-storage-attacher/tests/e2e/e2e-tests/utilscript.sh
@@ -46,7 +46,7 @@ then
     export CLSFILE=$1
     kubectl  create -f $CLSFILE
     #kubectl create -f $E2E_PATH/portworx_kp.yaml
-    kubectl create -f $E2E_PATH/portworx_secret.yaml
+    #kubectl create -f $E2E_PATH/portworx_secret.yaml
 elif [ "$2" = "portworxdelete" ]
 then
     sudo curl -fsL https://install.portworx.com/px-wipe | bash
